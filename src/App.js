cd5cd36d950756d80238ee7e150ca2e95940712d
Shuaib Khan <shuaib.middle@gmail.com>
2023-05-27 12:56:05 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import EditData from "./components/EditData/EditData";
+import Home from "./components/Home/Home";
+import RegisterData from "./components/RegisterData/RegisterData";
+import UserDetails from "./components/UserDetails/UserDetails";
+import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <>
+        <Router>
+        <Navbar/>
+<Routes>
+<Route exact path="/" element={<Home/>}/>
+<Route exact path="/register" element={<RegisterData/> }></Route>
+<Route exact path="/edit/:id" element={<EditData/>}/>
+<Route exact path="/user_details/:id" element={<UserDetails/>}></Route>
+</Routes>
+        </Router>
+            
+            
+            
+
+        </>
+    )
 }
 
 export default App;
